41c66b3c842d39fce842f6287be3463620820f75
kkimikia-ctrl <kkimikia@gmail.com>
2026-08-02 17:05:02 +0900

    Delete index.js.js

--- index.js.js
+++
@@ -1,8 +0,0 @@
-module.exports = async (req, res) => {
-  if (req.method === 'POST') {
-    // Responde com sucesso para o LINE (Status 200)
-    res.status(200).json({ status: 'success' });
-  } else {
-    res.status(200).json({ message: 'Bot está rodando!' });
-  }
-};
