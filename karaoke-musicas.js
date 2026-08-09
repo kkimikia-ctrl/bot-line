e5491ff1ae62a89a9fd33311f5217560242298ab
kkimikia-ctrl <kkimikia@gmail.com>
2026-08-09 16:39:34 +0900

    Update karaoke-musicas.js

--- karaoke-musicas.js
+++ karaoke-musicas.js
@@ -1,18 +1,18 @@
 const listaKaraoke = [
-    // --- Pop & Rock Nacional (Com links reais do YouTube) ---
-    { categoria: "Pop & Rock Nacional", titulo: "Piloto - Jão", link: "https://www.youtube.com/watch?v=K3uJ3V3rX88" },
-    { categoria: "Pop & Rock Nacional", titulo: "Idiota - Jão", link: "https://www.youtube.com/watch?v=7W5t6v1kL9o" },
-    { categoria: "Pop & Rock Nacional", titulo: "Pupila - Vitor Kley & ANAVITÓRIA", link: "https://www.youtube.com/watch?v=5s30W0wZt3o" },
-    { categoria: "Pop & Rock Nacional", titulo: "Pais e Filhos - Legião Urbana", link: "https://www.youtube.com/watch?v=5s30W0wZt3o" },
-    { categoria: "Pop & Rock Nacional", titulo: "Sutilmente - Skank", link: "https://www.youtube.com/watch?v=Gk5X3aK1N6M" },
-    { categoria: "Pop & Rock Nacional", titulo: "Pelados em Santos - Mamonas Assassinas", link: "https://www.youtube.com/watch?v=b1QZ6sD_w6c" },
-
     // --- Sertanejo & Modão ---
-    { categoria: "Sertanejo & Modão", titulo: "Evidências - Chitãozinho & Xororó", link: "https://www.youtube.com/watch?v=e9Z6x4g36Ao" },
-    { categoria: "Sertanejo & Modão", titulo: "Dormir na Praça - Bruno & Marrone", link: "https://www.youtube.com/watch?v=7zM6W6a1L9o" },
-    { categoria: "Sertanejo & Modão", titulo: "Infiel - Marília Mendonça", link: "https://www.youtube.com/watch?v=5Xp_2Wy6W2E" },
-
+    {
+        categoria: "Sertanejo & Modão",
+        titulo: "Não Aprendi Dizer Adeus - Leandro e Leonardo",
+        link: "https://youtu.be/VJmwbtDYyeQ"
+    },
+    
     // --- Pagode & Axé ---
-    { categoria: "Pagode & Axé", titulo: "Cheia de Manias - Raça Negra", link: "https://www.youtube.com/watch?v=6P3M8nZ0F2M" },
-    { categoria: "Pagode & Axé", titulo: "Deixa Acontecer - Grupo Revelação", link: "https://www.youtube.com/watch?v=7zM6W6a1L9o" }
+    // Adicione mais músicas abaixo seguindo o mesmo padrão:
+    /*
+    {
+        categoria: "Pagode & Axé",
+        titulo: "Nome da Música - Cantor",
+        link: "https://youtu.be/LINK_AQUI"
+    }
+    */
 ];
