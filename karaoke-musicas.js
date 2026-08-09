5e871cbac3caf33d14875c58f1e82765f87ddcc2
kkimikia-ctrl <kkimikia@gmail.com>
2026-08-09 17:57:23 +0900

    Update karaoke-musicas.js

--- karaoke-musicas.js
+++ karaoke-musicas.js
@@ -1,10 +1,17 @@
 const listaKaraoke = [
     // --- Sertanejo & Modão ---
     {
+       {
         categoria: "Sertanejo & Modão",
         titulo: "Não Aprendi Dizer Adeus - Leandro e Leonardo",
         link: "https://youtu.be/VJmwbtDYyeQ"
     },
+        {
+        categoria: "Sertanejo & Modão",
+        titulo: "Dormi na Praça - Bruno e Marrone",
+        link: "https://youtu.be/XE4Y2dyUntE" 
+    },
+
     
     // --- Pagode & Axé ---
     // Adicione mais músicas abaixo seguindo o mesmo padrão:
