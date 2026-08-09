f2eb02860066e45e935b863687ac94c508e1bcd3
kkimikia-ctrl <kkimikia@gmail.com>
2026-08-09 20:23:37 +0900

    Update karaoke-musicas.js

--- karaoke-musicas.js
+++ karaoke-musicas.js
@@ -5,6 +5,12 @@ const listaKaraoke = [
         titulo: "Não Aprendi Dizer Adeus - Leandro e Leonardo",
         link: "https://youtu.be/VJmwbtDYyeQ"
     },
+
+  {
+        categoria: "Sertanejo & Modão",
+        titulo: "Sorriso Forçado - Ana Castela",
+        link: "https://youtu.be/ib2f-LGQ4X8"
+    },
     {
         categoria: "Sertanejo & Modão",
         titulo: "Dormi na Praça - Bruno e Marrone",
