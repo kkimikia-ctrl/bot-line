6c20899e3172693a9d01935f7a32df84e1198bb4
kkimikia-ctrl <kkimikia@gmail.com>
2026-09-06 22:50:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify, render_template_string
 from tradutor import traduzir
 
@@ -81,4 +82,5 @@ def api_traduzir():
     return jsonify({'traducao': traducao, 'romaji': romaji})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=7860)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
